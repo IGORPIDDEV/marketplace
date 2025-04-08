80d4a9d2ebf1f7fc5c41ddd3f6ced01cdb33c611
igorpiddev <igorpiddev@gmail.com>
2025-04-08 15:19:24 +0300

    Configure migrations running

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -6,10 +6,14 @@ if [ ! -f artisan ]; then
     composer create-project laravel/laravel .
 fi
 
+echo "Installing Composer..."
 composer install
 php artisan key:generate
-php artisan migrate
 
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Installing Node..."
 npm install
 nohup npm run dev --prefix /var/www/html > /dev/null 2>&1 &
 
